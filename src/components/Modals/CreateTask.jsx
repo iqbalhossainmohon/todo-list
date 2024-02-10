07319b3a02dcbc8edeee9ae9bd8cb12e81768b09
iqbalhossainmohon <iqbalhossainmohon3057@gamil.com>
2024-02-11 02:16:58 +0600

    local storage data add

--- src/components/Modals/CreateTask.jsx
+++ src/components/Modals/CreateTask.jsx
@@ -2,45 +2,55 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-const CreateTask = ({ show, toggle }) => {
+const CreateTask = ({ show, toggle, save }) => {
+
+    const handleSave = (event) => {
+        event.preventDefault()
+        const taskName = event.target.taskName.value
+        const description = event.target.description.value
+        const priority = event.target.priority.value
+
+        let taskObj = {taskName, description, priority}
+        save(taskObj)
+
+        console.log(taskObj)
+    }
+
     return (
-        <Modal
-            show={show}
-            onHide={toggle}
-            backdrop="static"
-            keyboard={false}
-        >
+
+        <Modal show={show} onHide={toggle} backdrop="static" keyboard={false}>
             <Modal.Header closeButton>
                 <Modal.Title>Create Task</Modal.Title>
             </Modal.Header>
-
             <Modal.Body>
-                <form>
+                <form onSubmit={handleSave}>
                     <div className='form-group'>
                         <label>Task Name</label>
-                        <input className='form-control' type="text" />
+                        <input name='taskName' id='taskName' className='form-control' type="text"
+                        />
                     </div>
                     <div className='form-group mt-2'>
-                    <label>Description</label>
-                        <textarea className='form-control' name="" rows="3"></textarea>
+                        <label>Description</label>
+                        <textarea name='description' id='description' className='form-control' rows="3"></textarea>
                     </div>
                     <div className='form-group mt-2'>
                         <label>Priority</label>
-                        <select className="form-select">
-                            <option value="1">Low</option>
-                            <option value="2">Medium</option>
-                            <option value="3">High</option>
+                        <select className="form-select" name='priority' id='priority'>
+                            <option value="Low">Low</option>
+                            <option value="Medium">Medium</option>
+                            <option value="High">High</option>
                         </select>
                     </div>
+
+                    <Modal.Footer>
+                        <Button type='submit' variant="primary" >Create</Button>
+                        <Button variant="secondary" onClick={toggle}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
                 </form>
             </Modal.Body>
 
-            <Modal.Footer>
-                <Button variant="primary">Create</Button>
-                <Button variant="secondary" onClick={toggle}>
-                    Close
-                </Button>
-            </Modal.Footer>
         </Modal>
     );
 };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,21 +4,45 @@ import CreateTask from './Modals/CreateTask';
 const TodoList = () => {
 
     const [show, setShow] = useState(false);
-    const toggle =()=>{
+    const [isComplited, setIsComplited] = useState(false);
+    const [taskList, setTaskList] = useState([])
+
+    const toggle = () => {
         setShow(!show);
+    }
+
+    const saveTask = (taskObj)=>{
+        let tempList = taskList
+        tempList.push(taskObj)
+        localStorage.setItem("Task-List", JSON.stringify(tempList))
+        setTaskList(tempList)
+        setShow(false)
     }
 
     return (
         <>
             <div className='header text-center pt-5'>
                 <h1 className='header-title'>Todo List</h1>
-                <button onClick={()=>setShow(true)} className='btn btn-primary mt-2'>Create Todo</button>
+
+                <div className='d-flex justify-content-evenly mt-2'>
+                    <div className=''>
+                        <button className={`toggleButton ${isComplited === false && 'active'}`} onClick={() => setIsComplited(false)}>Incomplited</button>
+                        <button className={`toggleButton ${isComplited === true && 'active'}`} onClick={() => setIsComplited(true)}>Complited</button>
+                    </div>
+
+                    <div>
+                        <button onClick={() => setShow(true)} className='btn btn-primary mt-2'>Create Task</button>
+                    </div>
+                </div>
             </div>
 
             <div className='task-container'>
 
+                {/* <-- to-do list card -->   */}
+                {taskList.map((obj)=> <li>{obj.taskName}</li>)}
+
             </div>
-            <CreateTask show={show} toggle={toggle} />
+            <CreateTask show={show} setShow={setShow} toggle={toggle} save={saveTask} />
         </>
     );
 };
