62223c5badee56940404b249c9435e8dcfa2d96a
iqbalhossainmohon <iqbalhossainmohon3057@gamil.com>
2024-02-13 12:04:31 +0600

    mod card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,23 +4,23 @@ import { RiEditBoxFill } from "react-icons/ri";
 import { RiDeleteBin6Fill } from "react-icons/ri";
 import EditTask from './Modals/EditTask';
 
-const Card = ({ taskObj, index, deleteTask, updateListArray }) => {
+const Card = ({ taskObj, index, deleteTask, updateListArray, handleStatus }) => {
     const [show, setShow] = useState(false);
 
-    const toggle = ()=>{
+    const toggle = () => {
         setShow(!show)
     }
 
-    const updateTask = (obj)=>{
+    const updateTask = (obj) => {
         updateListArray(obj, index)
     }
 
-    const handleDelete = ()=>{
+    const handleDelete = () => {
         deleteTask(index)
     }
 
-    return (
-        <div className='container mt-3 bg-body-secondary rounded p-3'>
+    return ( 
+            <div className='container mt-3 bg-body-secondary rounded p-3'>
             <div className='card-top' style={{ "background-color": "chartreuse" }}>
                 
             </div>
@@ -30,16 +30,21 @@ const Card = ({ taskObj, index, deleteTask, updateListArray }) => {
                     <p>{taskObj.description}</p>
                     <p><span className='text-primary '>Priority:</span> {taskObj.priority}</p>
                 </div>
-                <div className='position-absolute bottom-0 end-0'>
-                    <FaCheckCircle size={24} style={{"color": "Green", "margin": "7px"}}/>
-                    <RiEditBoxFill size={24} style={{"color": "Green", "margin": "7px"}} 
-                    onClick={()=>setShow(true)} />
-                    <RiDeleteBin6Fill size={24} style={{"color": "red", "margin": "7px"}} 
-                    onClick={handleDelete} />
+                <div className='actionBtn position-absolute bottom-0 end-0'>
+
+                    <FaCheckCircle size={24} title='Complete' style={{ "color": "Green", "margin": "7px" }}
+                        onClick={() => handleStatus(taskObj.id)}
+                    />
+
+                    <RiEditBoxFill size={24} title='Edit' style={{ "color": "Green", "margin": "7px" }}
+                        onClick={() => setShow(true)} />
+                    <RiDeleteBin6Fill size={24} title='Delete' style={{ "color": "red", "margin": "7px" }}
+                        onClick={handleDelete} />
                 </div>
             </div>
             <EditTask show={show} toggle={toggle} updateTask={updateTask} taskObj={taskObj} />
         </div>
+
     );
 };
 
