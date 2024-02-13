868bde6f461ea061b31fae952b276399d1e49d0e
iqbalhossainmohon <iqbalhossainmohon3057@gamil.com>
2024-02-13 12:04:57 +0600

    mod todolist.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import CreateTask from './Modals/CreateTask';
 import Card from './Card';
+import CompleteCard from './CompleteCard';
 
 const TodoList = () => {
 
     const [show, setShow] = useState(false);
     const [isComplited, setIsComplited] = useState(false);
     const [taskList, setTaskList] = useState([])
+    const [complitedTodos, setComplitedTodos] = useState([])
 
 
     useEffect(() => {
@@ -45,31 +47,93 @@ const TodoList = () => {
         setShow(false)
     }
 
+    const handleStatus = (id, index) => {
+        const items = JSON.parse(localStorage.getItem("Task-List"));
+
+        if (items) {
+            const foundIndex = items.findIndex(item => item.id === id);
+            if (foundIndex !== -1) {
+                items[foundIndex].isComplete = true;
+
+                localStorage.setItem("Task-List", JSON.stringify(items));
+
+            } else {
+                console.log("Item not found");
+            }
+        } else {
+            console.log("No items found in localStorage");
+        }
+
+        console.log(id)
+
+        let now = new Date();
+        let dd = now.getDate();
+        let mm = now.getMonth() + 1;
+        let yyyy = now.getFullYear();
+        let h = now.getHours();
+        let m = now.getMinutes();
+        let s = now.getSeconds();
+        let completedOn = dd + '-' + mm + '-' + yyyy + ' at ' + h + ':' + m + ':' + s;
+
+        let filteredItem = {
+            ...taskList[index],
+            completedOn: completedOn,
+            taskList: taskList
+            
+        }
+        let updatedCompletedArr = [...complitedTodos];
+        updatedCompletedArr.push(filteredItem);
+
+        setComplitedTodos(updatedCompletedArr);
+
+    }
+
     return (
         <>
             <div className='header text-center pt-5'>
                 <h1 className='header-title'>Todo List</h1>
 
-                <div className='d-flex justify-content-evenly mt-2'>
-                    <div className=''>
+                <div className='d-flex justify-content-evenly'>
+                    {/* <div className=''>
                         <button className={`toggleButton ${isComplited === false && 'active'}`} onClick={() => setIsComplited(false)}>Incomplited</button>
+
                         <button className={`toggleButton ${isComplited === true && 'active'}`} onClick={() => setIsComplited(true)}>Complited</button>
-                    </div>
+                    </div> */}
                     <div>
-                        <button onClick={() => setShow(true)} className='btn btn-primary '>Create Task</button>
+                        <button onClick={() => setShow(true)} className='btn btn-primary mt-2'>Create Task</button>
                     </div>
                 </div>
             </div>
 
             <div className='task-container'>
 
+                <div className='d-flex justify-content-evenly mt-2'>
+                    <button className={`toggleButton ${isComplited === false && 'active'}`} onClick={() => setIsComplited(false)}>Incomplited</button>
+
+                    <button className={`toggleButton ${isComplited === true && 'active'}`} onClick={() => setIsComplited(true)}>Complited</button>
+                </div>
+
                 {
-                    taskList.map((obj, index) => <Card taskObj={obj} index={index}
-                        deleteTask={deleteTask} updateListArray={updateListArray} />)
+                    isComplited === false && taskList.map((obj, index) => <Card taskObj={obj} index={index}
+                        deleteTask={deleteTask} updateListArray={updateListArray}
+                        handleStatus={handleStatus} />)
+                }
+
+                {/* {
+                   isComplited === true && complitedTodos.map((obj, index) => <Card taskObj={obj} index={index}
+                        deleteTask={deleteTask} updateListArray={updateListArray} 
+                        handleStatus={handleStatus} />)
+                } */}
+                {
+                    isComplited === true && complitedTodos.map((obj, index) => <CompleteCard taskObj={obj} index={index}
+                        deleteTask={deleteTask} updateListArray={updateListArray}
+                        handleStatus={handleStatus} id={obj.id}
+                    />)
                 }
 
             </div>
-            <CreateTask show={show} setShow={setShow} toggle={toggle} save={saveTask} />
+            <CreateTask show={show} setShow={setShow} toggle={toggle} save={saveTask}
+            />
         </>
     );
 };
