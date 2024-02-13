3aa23840aaeb52125530ee93a6b11e140168c958
iqbalhossainmohon <iqbalhossainmohon3057@gamil.com>
2024-02-13 12:04:44 +0600

    add completeCard.jsx

---
+++ src/components/CompleteCard.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { RiDeleteBin6Fill } from "react-icons/ri";
+import EditTask from './Modals/EditTask';
+
+const CompleteCard = ({ taskObj, index, deleteTask, updateListArray, handleStatus, id }) => {
+    const [show, setShow] = useState(false);
+
+    const toggle = () => {
+        setShow(!show)
+    }
+
+    const updateTask = (obj) => {
+        updateListArray(obj, index)
+    }
+
+    const handleDelete = () => {
+        deleteTask(index)
+    }
+
+    console.log(taskObj)
+
+    // const handleStatus = () => {
+    //     console.log("hellow");
+    // }
+
+    return (
+
+        <div className='container mt-3 bg-body-secondary rounded p-3'>
+            <div className='card-top' style={{ "background-color": "chartreuse" }}>
+
+            </div>
+            <div class="d-flex justify-content-between position-relative">
+                <div className='task-header'>
+                    <span className='card-header fs-1 fw-medium font-' >{taskObj.taskName}</span>
+                    <p>{taskObj.description}</p>
+                    <p><span className='text-primary '>Priority:</span> {taskObj.priority}</p>
+                    <p><small>Completed at: </small>{taskObj.completedOn}</p>
+                </div>
+                <div className='actionBtn position-absolute bottom-0 end-0'>
+
+
+
+                    {/* <RiEditBoxFill size={24} style={{ "color": "Green", "margin": "7px" }}
+                        onClick={() => setShow(true)} /> */}
+
+                    <RiDeleteBin6Fill size={24} title='Delete' style={{ "color": "red", "margin": "7px" }}
+                        onClick={handleDelete} />
+                </div>
+            </div>
+            <EditTask show={show} toggle={toggle} updateTask={updateTask} taskObj={taskObj} />
+        </div>
+
+    );
+};
+
+export default CompleteCard;
